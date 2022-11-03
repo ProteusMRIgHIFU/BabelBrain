18af3f936833c60f8b49420daecb90d49d8293c5
spichardo <samuel.pichardo@ucalgary.ca>
2022-11-02 18:28:04 -0600

    add script to create dmg installer

---
+++ BabelBrain/create_dmg.sh
@@ -0,0 +1,14 @@
+# Since create-dmg does not clobber, be sure to delete previous DMG
+[[ -f BabelBrain.dmg ]] && rm BabelBrain.dmg
+
+# Create the DMG
+create-dmg \
+  --volname "BabelBrain Installer" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "BabelBrain.app" 200 190 \
+  --hide-extension "BabelBrain.app" \
+  --app-drop-link 600 185 \
+  "BabelBrain.dmg" \
+  "dist/"
