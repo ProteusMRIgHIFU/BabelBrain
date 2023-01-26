2918151879c24657a2d86390e2ffc9e0caf64f3c
shirsks14 <shirsks14@gmail.com>
2023-01-26 05:53:28 -0700

    trying to fix eol and binary flags

--- BabelBrain/ExternalBin/elastix/run_linux.sh
+++ BabelBrain/ExternalBin/elastix/run_linux.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-target="/linux/lib/"
 echo ${SCRIPT_DIR}
 OLD_LD=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH="${SCRIPT_DIR}${target}":$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/linux/lib/":$LD_LIBRARY_PATH
 "$SCRIPT_DIR/linux/bin/elastix"  -f "$1" -m "$2" -out "$3"  -p "$SCRIPT_DIR/rigid.txt"
 output=$?
 export LD_LIBRARY_PATH=$OLD_LD
