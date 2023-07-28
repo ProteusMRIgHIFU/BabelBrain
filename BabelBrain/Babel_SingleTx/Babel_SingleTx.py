ce03a0a33000571a1c6e8b444de974020748d6f1
acoreas <saeroc@outlook.com>
2023-07-28 16:10:31 -0600

    improved speed of step 2 scrollbars, but have issue with plot placement

--- BabelBrain/Babel_SingleTx/Babel_SingleTx.py
+++ BabelBrain/Babel_SingleTx/Babel_SingleTx.py
@@ -276,53 +276,75 @@ class SingleTx(QWidget):
             self._ISkull = ISkull
             self._DistanceToTarget = DistanceToTarget
         
-        if hasattr(self,'_figAcField'):
-                children = []
-                for i in range(self._layout.count()):
-                    child = self._layout.itemAt(i).widget()
-                    if child:
-                        children.append(child)
-                for child in children:
-                    child.deleteLater()
-                delattr(self,'_figAcField')
-                self.Widget.AcField_plot1.repaint()
-        self._figAcField=Figure(figsize=(14, 12))
-
-        SelY, SelX = self.Widget.IsppaScrollBars.get_scroll_values()
-
-        if not hasattr(self,'_layout'):
-           self._layout = QVBoxLayout(self.Widget.AcField_plot1)
-
-        self.static_canvas = FigureCanvas(self._figAcField)
-        toolbar=NavigationToolbar2QT(self.static_canvas,self)
-        self._layout.addWidget(toolbar)
-        self._layout.addWidget(self.static_canvas)
-        static_ax1,static_ax2 = self.static_canvas.figure.subplots(1,2)
-
+            if hasattr(self,'_figAcField'):
+                    children = []
+                    for i in range(self._layout.count()):
+                        child = self._layout.itemAt(i).widget()
+                        if child:
+                            children.append(child)
+                    for child in children:
+                        child.deleteLater()
+                    delattr(self,'_figAcField')
+                    self.Widget.AcField_plot1.repaint()
+        
         if self.Widget.ShowWaterResultscheckBox.isChecked():
             Field=self._IWater
         else:
             Field=self._ISkull
 
-        self._imContourf1=static_ax1.contourf(self._XX,self._ZZX,Field[:,SelY,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
-        h=plt.colorbar(self._imContourf1,ax=static_ax1)
-        h.set_label('$I_{\mathrm{SPPA}}$ (normalized)')
-        static_ax1.contour(self._XX,self._ZZX,self._Skull['MaterialMap'][:,SelY,:].T,[0,1,2,3], cmap=plt.cm.gray)
-        static_ax1.set_aspect('equal')
-        static_ax1.set_xlabel('X mm')
-        static_ax1.set_ylabel('Z mm')
-        static_ax1.invert_yaxis()
-        static_ax1.plot(0,self._DistanceToTarget,'+y',markersize=18)
+        SelY, SelX = self.Widget.IsppaScrollBars.get_scroll_values()
 
-        self._imContourf2=static_ax2.contourf(self._YY,self._ZZY,Field[SelX,:,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
-        h=plt.colorbar(self._imContourf1,ax=static_ax2)
-        h.set_label('$I_{\mathrm{SPPA}}$ (normalized)')
-        static_ax2.contour(self._YY,self._ZZY,self._Skull['MaterialMap'][SelX,:,:].T,[0,1,2,3], cmap=plt.cm.gray)
-        static_ax2.set_aspect('equal')
-        static_ax2.set_xlabel('Y mm')
-        static_ax2.set_ylabel('Z mm')
-        static_ax2.invert_yaxis()
-        static_ax2.plot(0,self._DistanceToTarget,'+y',markersize=18)
+        if hasattr(self,'_figAcField'):
+            if hasattr(self,'_imContourf1'):
+                for c in [self._imContourf1,self._imContourf2,self._contour1,self._contour2]:
+                    for coll in c.collections:
+                        coll.remove()
+                del self._imContourf1
+                del self._imContourf2
+                del self._contour1
+                del self._contour2
+
+            self._imContourf1=self._static_ax1.contourf(self._XX,self._ZZX,Field[:,SelY,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
+            self._contour1 = self._static_ax1.contour(self._XX,self._ZZX,self._Skull['MaterialMap'][:,SelY,:].T,[0,1,2,3], cmap=plt.cm.gray)
+
+            self._imContourf2=self._static_ax2.contourf(self._YY,self._ZZY,Field[SelX,:,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
+            self._contour2 = self._static_ax2.contour(self._YY,self._ZZY,self._Skull['MaterialMap'][SelX,:,:].T,[0,1,2,3], cmap=plt.cm.gray)
+
+            self._figAcField.canvas.draw_idle()
+        else:
+            self._figAcField=Figure(figsize=(14, 12))
+
+            if not hasattr(self,'_layout'):
+                self._layout = QVBoxLayout(self.Widget.AcField_plot1)
+
+            self.static_canvas = FigureCanvas(self._figAcField)
+            toolbar=NavigationToolbar2QT(self.static_canvas,self)
+            self._layout.addWidget(toolbar)
+            self._layout.addWidget(self.static_canvas)
+            static_ax1,static_ax2 = self.static_canvas.figure.subplots(1,2)
+            self._static_ax1 = static_ax1
+            self._static_ax2 = static_ax2
+
+            self._imContourf1=static_ax1.contourf(self._XX,self._ZZX,Field[:,SelY,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
+            h=plt.colorbar(self._imContourf1,ax=static_ax1)
+            h.set_label('$I_{\mathrm{SPPA}}$ (normalized)')
+            self._contour1 = static_ax1.contour(self._XX,self._ZZX,self._Skull['MaterialMap'][:,SelY,:].T,[0,1,2,3], cmap=plt.cm.gray)
+            static_ax1.set_aspect('equal')
+            static_ax1.set_xlabel('X mm')
+            static_ax1.set_ylabel('Z mm')
+            static_ax1.invert_yaxis()
+            static_ax1.plot(0,self._DistanceToTarget,'+y',markersize=18)
+
+            self._imContourf2=static_ax2.contourf(self._YY,self._ZZY,Field[SelX,:,:].T,np.arange(2,22,2)/20,cmap=plt.cm.jet)
+            h=plt.colorbar(self._imContourf1,ax=static_ax2)
+            h.set_label('$I_{\mathrm{SPPA}}$ (normalized)')
+            self._contour2 = static_ax2.contour(self._YY,self._ZZY,self._Skull['MaterialMap'][SelX,:,:].T,[0,1,2,3], cmap=plt.cm.gray)
+            static_ax2.set_aspect('equal')
+            static_ax2.set_xlabel('Y mm')
+            static_ax2.set_ylabel('Z mm')
+            static_ax2.invert_yaxis()
+            static_ax2.plot(0,self._DistanceToTarget,'+y',markersize=18)
+
         self._figAcField.set_facecolor(np.array(self.Widget.palette().color(QPalette.Window).getRgb())/255)
         self._figAcField.set_tight_layout(True)
 
