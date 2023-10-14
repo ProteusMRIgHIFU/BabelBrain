27f35cce0d38fe56e9bcce97f7d25d917da4cfa7
spichardo <samuel.pichardo@ucalgary.ca>
2023-10-14 08:14:59 -0600

    it seems signed package working for arm64

---
+++ BabelBrain/create_dmg_ARM64.sh
@@ -0,0 +1,19 @@
+# Since create-dmg does not clobber, be sure to delete previous DMG
+[[ -f BabelBrain_ARM64.dmg ]] && rm BabelBrain_ARM64.dmg
+rm -rf final
+mkdir final
+mkdir final/ThermalProfiles
+mkdir final/PlanningModels
+cp -r ../ThermalProfiles/* final/ThermalProfiles
+cp -r ../PlanningModels/* final/PlanningModels
+cp -r ../LICENSE final/
+cp BabelBrain_ARM64.pkg final/
+# Create the DMG   --hide-extension "BabelBrain.app" \  --app-drop-link 600 185 \
+create-dmg \
+  --volname "BabelBrain Installer" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "BabelBrain_ARM64.pkg" 200 190 \
+  "BabelBrain_ARM64.dmg" \
+  "final/"
---
+++ BabelBrain/create_signed_arm64.sh
@@ -0,0 +1,7 @@
+#script to run all steps to create app, package , dmg , codesign and notarize
+rm -rf dist build 
+pyinstaller BabelBrain.spec
+python pycodesign.py -s -p  -n -t "/Users/spichardo/Library/Mobile Documents/com~apple~CloudDocs/pycodesign/pycodesign_arm64.ini"
+source create_dmg_ARM64.sh
+source "/Users/spichardo/Library/Mobile Documents/com~apple~CloudDocs/pycodesign/notarize_dmg_arm64.sh"
+xcrun stapler staple BabelBrain_ARM64.dmg
