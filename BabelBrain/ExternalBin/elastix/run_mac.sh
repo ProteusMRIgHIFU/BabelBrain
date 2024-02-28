39cebdc3a8027f04901b8c059ca8ddb67902127c
spichardo <samuel.pichardo@ucalgary.ca>
2024-02-28 13:47:16 -0700

    changing to make Brainsight to be able to run the elastix step
    otherwise it hangs

--- BabelBrain/ExternalBin/elastix/run_mac.sh
+++ BabelBrain/ExternalBin/elastix/run_mac.sh
@@ -1,10 +1,14 @@
-SCRIPT_DIR="${0:a:h}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 target="/mac/lib/"
 echo ${SCRIPT_DIR}
 OLD_DYLD=$DYLD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH="${SCRIPT_DIR}${target}":$DYLD_LIBRARY_PATH
-"$SCRIPT_DIR/mac/bin/elastix"  -f "$1" -m "$2" -out "$3"  -p "$SCRIPT_DIR/rigid.txt"
+"$SCRIPT_DIR/mac/bin/elastix" -loglevel off -f "$1" -m "$2" -out "$3"  -p "$SCRIPT_DIR/rigid.txt" > /dev/null
 output=$?
 export DYLD_LIBRARY_PATH=$OLD_DYLD
 echo "elastix ended with code " $output
-exit output
+if [ $output -eq 0 ]; then
+    true
+else
+    false
+fi
