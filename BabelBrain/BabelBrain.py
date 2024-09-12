Fix MI calculation

BabelBrain/BabelBrain.py
@@ -694,8 +694,12 @@ class BabelBrain(QWidget):
         '''
         Frequency=  self.Widget.USMaskkHzDropDown.property('UserData')
         BasePPW=self.Widget.USPPWSpinBox.property('UserData')
+        self._Frequency =Frequency
+        self._BasePPW =BasePPW
 
         self._prefix= self.Config['ID'] + '_' + self.Config['TxSystem'] +'_%ikHz_%iPPW_' %(int(Frequency/1e3),BasePPW)
+        
+        
         basedir = self.Config['OutputFilesPath']
         if not os.path.isdir(basedir):
             try:
@@ -955,7 +959,8 @@ class BabelBrain(QWidget):
           
     def GetExport(self):
         ExtraConfig ={}
-        ExtraConfig['PPW']=self.Widget.USPPWSpinBox.property('UserData')
+        ExtraConfig['Frequency']=self._Frequency
+        ExtraConfig['PPW']=self._BasePPW
         if self.Config['bUseCT']:
             ExtraConfig['HUThreshold']=self.Widget.HUTreshold.value()
             if self.Config['CTType'] in [2,3]: #ZTE or PETRA
@@ -1018,10 +1023,10 @@ class RunMaskGeneration(QObject):
         T1WIso= self._mainApp.Config['T1WIso']
         T1W= self._mainApp.Config['T1W']
 
-        Frequency=  Widget.USMaskkHzDropDown.property('UserData')
+        Frequency=  self._mainApp._Frequency
         SmallestSoS= GetSmallestSOS(Frequency,bShear=True)
 
-        BasePPW=Widget.USPPWSpinBox.property('UserData')
+        BasePPW=self._BasePPW
         SpatialStep=np.round(SmallestSoS/Frequency/BasePPW*1e3,3) #step of mask to reconstruct , mm
         print("Frequency, SmallestSoS, BasePPW,SpatialStep",Frequency, SmallestSoS, BasePPW,SpatialStep)
 

BabelBrain/Babel_H246/Babel_H246.py
@@ -215,8 +215,8 @@ class RunAcousticSim(QObject):
         ZSteering=TPODistance
         print('ZSteering',ZSteering*1e3)
 
-        Frequencies = [self._mainApp.Widget.USMaskkHzDropDown.property('UserData')]
-        basePPW=[self._mainApp.Widget.USPPWSpinBox.property('UserData')]
+        Frequencies = [self._mainApp._Frequency)]
+        basePPW=[self._mainApp._BasePPW]
         ZIntoSkin =0.0
         if TxMechanicalAdjustmentZ > 0:
             ZIntoSkin = np.abs(TxMechanicalAdjustmentZ)

BabelBrain/Babel_REMOPD/Babel_REMOPD.py
@@ -250,9 +250,9 @@ class RunAcousticSim(QObject):
         RotationZ=self._mainApp.AcSim.Widget.ZRotationSpinBox.value()
         TxSet = self._mainApp.AcSim.Widget.SelTxSetDropDown.currentText()
 
-        Frequencies = [self._mainApp.Widget.USMaskkHzDropDown.property('UserData')]
+        Frequencies = [self._mainApp._Frequency]
 
-        basePPW=[self._mainApp.Widget.USPPWSpinBox.property('UserData')]
+        basePPW=[self._mainApp._BasePPW]
         ZIntoSkin =0.0
         if TxMechanicalAdjustmentZ > 0:
             ZIntoSkin = np.abs(TxMechanicalAdjustmentZ)

BabelBrain/Babel_SingleTx/Babel_SingleTx.py
@@ -237,8 +237,8 @@ class RunAcousticSim(QObject):
         if CurDistance < 0:
             ZIntoSkin = np.abs(CurDistance)
 
-        Frequencies = [self._mainApp.Widget.USMaskkHzDropDown.property('UserData')]
-        basePPW=[self._mainApp.Widget.USPPWSpinBox.property('UserData')]
+        Frequencies = [self._mainApp._Frequency]
+        basePPW=[self._mainApp._BasePPW]
         T0=time.time()
         kargs={}
         kargs['extrasuffix']=self._extrasuffix